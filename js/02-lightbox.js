f5b39d2bb51515f0c2d5234d21d9289407ff9032
natalilia <nata.semenii@gmail.com>
2023-10-28 22:55:45 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+
+
+function createMarkup(arr){
+    return arr
+    .map(({preview, original, description}) =>
+    ` <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>
+  `,
+   
+    ) 
+    .join("");
+}
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: "250",
+});
